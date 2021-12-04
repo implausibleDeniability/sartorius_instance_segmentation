6aa3814083f5e1699fd3ce0045ca1f75aba6936b
Maxim Faleev <m.faleev@innopolis.university>
2021-12-04 11:22:44 +0000

    moved class CellDataset and utils to outer package

---
+++ scripts/dataset.py
@@ -0,0 +1,71 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset
+from easydict import EasyDict
+import torch
+import numpy as np
+from skimage import io
+import albumentations as A
+
+from scripts.utils import annotation2mask, get_box
+
+
+class CellDataset(Dataset):
+    def __init__(self, cfg: EasyDict, mode: str, transform: A.Compose = None):
+        assert mode == 'train' or mode == 'test'
+
+        self.transform = transform
+        self.image_folder = cfg.dataset_path / "train"
+
+        data_csv = pd.read_csv(cfg.dataset_path / "train.csv")
+        data_csv = data_csv[['id', 'annotation', 'cell_type']]
+        data_csv = data_csv.groupby(['id', 'cell_type'])['annotation'].agg(lambda x: list(x)).reset_index()
+
+        # Now data_csv has 606 rows with 3 columns:
+        # - id - id of image,
+        # - cell_type - type of cell,
+        # - annotation - list of rle strings
+        if mode == 'train':
+            self.data_csv, _ = train_test_split(data_csv, test_size=cfg.val_size, random_state=0)
+        else:
+            _, self.data_csv = train_test_split(data_csv, test_size=cfg.val_size, random_state=0)
+
+    def __getitem__(self, item):
+        # TODO: Test how much time does .iloc take
+        # if I (maxim) am not mistaken, that can be quite long
+        image_id, _, annotations = self.data_csv.iloc[item]
+        image_path = self.image_folder / (image_id + ".png")
+        image = io.imread(str(image_path))
+
+        # TODO: consider using rle_decode instead of annotation2mask
+        masks = list(map(lambda line: annotation2mask(line), annotations))
+        boxes = list(map(lambda mask: get_box(mask), masks))
+        labels = list(map(lambda _: 1, masks))
+
+        if self.transform:
+            transformed = self.transform(image=image, masks=masks, bboxes=boxes, category_ids= labels)
+
+            image = transformed['image']
+            masks = transformed['masks']
+            boxes = transformed['bboxes']
+
+        boxes = np.asarray(boxes)
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+
+        # Training on all samples (setting iscrown=False)
+        iscrowd = torch.zeros((len(masks),), dtype=torch.int64)
+
+        target = {
+            'masks': torch.as_tensor(masks),
+            'labels': torch.as_tensor(labels, dtype=torch.int64),
+            'boxes': torch.as_tensor(boxes),
+            'iscrowd': iscrowd,
+            'area': torch.as_tensor(area),
+            'image_id': image_id,  # For logging purposes
+        }
+
+
+        return image, target
+
+    def __len__(self):
+        return len(self.data_csv)
--- scripts/utils.py
+++ scripts/utils.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 from dotenv import load_dotenv
 
+
 def annotation2mask(annotation: str) -> np.ndarray:
     """Converts annotation string of the image 520x704 to np.ndarray 520x704"""
     # -1 because they numerate from 1, not from 0
@@ -17,3 +18,13 @@ def annotation2mask(annotation: str) -> np.ndarray:
         flatten_mask[segment_starts[i]:segment_ends[i]] = 1
     mask = flatten_mask.reshape([image_height, image_width])
     return mask
+
+
+def get_box(mask: np.array) -> list:
+    """ Get the bounding box of a given mask """
+    y_coords, x_coords = np.where(mask)
+    xmin = np.min(x_coords)
+    xmax = np.max(x_coords)
+    ymin = np.min(y_coords)
+    ymax = np.max(y_coords)
+    return [xmin, ymin, xmax, ymax]
