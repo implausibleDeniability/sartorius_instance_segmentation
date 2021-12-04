550732f7ed108da3459e38b1187ccbe399deb8b1
Maxim Faleev <m.faleev@innopolis.university>
2021-12-04 11:53:37 +0000

    visualization moved from notebook to the package

---
+++ scripts/visualization.py
@@ -0,0 +1,37 @@
+import torch
+import numpy as np
+from functools import reduce
+from matplotlib import pyplot as plt
+import cv2
+
+def show_image(im: torch.Tensor, boxes, masks):
+    """
+    Args:
+        im: torch.Tensor with shape [1, height, width]
+        boxes: torch.Tensor with shape [N, 4] with N being number of boxes
+        masks: torch.Tensor with shape [N, height, width]
+        
+    Visualization instance of dataset after augmentation
+    """
+    # torch image preprocessed for plotting with matplotlib
+    im = np.transpose(im.numpy(), axes=(1, 2, 0))
+    im = (im - im.min()) / (im.max() - im.min())
+    im = np.asarray(im * 256., dtype=np.uint8)
+
+    image_mask = reduce(lambda x, y: x + y, masks)
+
+    image_mask[image_mask > 1] = 1
+    yellow_mask = np.stack([image_mask, image_mask, np.zeros_like(image_mask)], axis=2)
+
+    image_with_mask = np.array(im + 50 * yellow_mask, dtype=np.uint8)
+
+    # Drawing red rectangle for each instances
+    red_color = (255, 0, 0)
+    for x1, y1, x2, y2 in boxes:
+        image_with_mask = cv2.rectangle(image_with_mask.copy(), pt1=(int(x1), int(y1)), pt2=(int(x2), int(y2)), color=red_color, thickness=2)
+    
+    plt.figure(figsize=(15, 15))
+    plt.imshow(image_with_mask)
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
