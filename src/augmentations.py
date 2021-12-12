536533a71f3ce7efaaa65ba1e34f29d1173f99e7
Maxim Faleev <m.faleev@innopolis.university>
2021-12-12 19:38:43 +0000

    ! eval augmentation

--- src/augmentations.py
+++ src/augmentations.py
@@ -4,4 +4,4 @@ from albumentations.pytorch import ToTensorV2
 eval_transform = A.Compose([
     A.Normalize(mean=(0.485,), std=(0.229,)),
     ToTensorV2(),
-], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['category_ids']))
+])
