e50e47885369f0cb2f1bd811685e564c612a17e4
homomorfism <arsl.sh056@mail.ru>
2021-12-13 18:37:07 +0300

    Visualisation of coco dataset - adding COCO dataset, notebook with visualisation, requirements packages

--- src/augmentations.py
+++ src/augmentations.py
@@ -5,3 +5,8 @@ eval_transform = A.Compose([
     A.Normalize(mean=(0.485,), std=(0.229,)),
     ToTensorV2(),
 ])
+
+train_transform = A.Compose([
+    A.Normalize(mean=(0.485,), std=(0.229,)),
+    ToTensorV2(),
+], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['category_ids']))
--- src/dataset.py
+++ src/dataset.py
@@ -1,8 +1,14 @@
+import json
+from collections import defaultdict
+from pathlib import Path
+
 import albumentations as A
 import numpy as np
 import pandas as pd
 import torch
 from easydict import EasyDict
+from rasterio.features import rasterize
+from shapely.geometry import Polygon
 from skimage import io
 from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset
@@ -43,7 +49,7 @@ class CellDataset(Dataset):
         labels = list(map(lambda _: 1, masks))
 
         if self.transform:
-            transformed = self.transform(image=image, masks=masks, bboxes=boxes, category_ids= labels)
+            transformed = self.transform(image=image, masks=masks, bboxes=boxes, category_ids=labels)
 
             image = transformed['image']
             masks = transformed['masks']
@@ -64,8 +70,118 @@ class CellDataset(Dataset):
             # 'image_id': image_id,  # For logging purposes
         }
 
-
         return image, target
 
     def __len__(self):
         return len(self.data_csv)
+
+
+def preprocess_annotations(data: dict) -> (dict, dict, dict):
+    """Returns dictionaries that have as a key ids of images that maps to segmentations, boxes and areas
+    Args:
+        data - coco json dict
+
+    Returns:
+        dict[image_id] -> list of contours' masks
+        dict[image_id] -> list of areas of each box
+        dict[image_id] -> list of outer boxes
+    """
+    image_to_segmentation = defaultdict(list)
+    image_to_boxes = defaultdict(list)
+    image_to_area = defaultdict(list)
+    for segm_id, metadata in data['annotations'].items():
+        image_id = metadata['image_id']
+        image_to_segmentation[image_id].append(metadata['segmentation'][0])
+        image_to_area[image_id].append(metadata['area'])
+        image_to_boxes[image_id].append(metadata['bbox'])
+
+    return image_to_segmentation, image_to_area, image_to_boxes
+
+
+def contours_to_masks(contours: list):
+    """Converts COCO format of masks (polygons) to binary masks"""
+    masks = []
+    for segmentation in contours:
+        segmentation = np.asarray(segmentation).reshape(-1, 2)
+        polygon = Polygon(segmentation)
+        mask = rasterize([polygon], out_shape=(520, 704))
+        masks.append(mask)
+
+    masks = np.asarray(masks)
+    return masks
+
+
+def xywh2xyxy(boxes):
+    """Converts COCO format boxes (x_min, y_min, height, width) for passing to model (x_min, y_min, x_max, y_max)"""
+
+    xmin = boxes[:, 0]
+    ymin = boxes[:, 1]
+    boxes[:, 2] += xmin
+    boxes[:, 3] += ymin
+
+    return boxes
+
+
+class COCODataset(Dataset):
+    """Dataset for reading COCO annotations format"""
+
+    def __init__(self, image_dir: Path, annotation_file: Path, transform: A.Compose = None):
+        super(COCODataset, self).__init__()
+
+        self.transform = transform
+
+        assert annotation_file.is_file()
+        assert image_dir.is_dir()
+
+        self.image_paths = {image.name: image.absolute() for image in image_dir.glob("**/*.*")}
+
+        with open(annotation_file) as file:
+            self.annotations = json.load(file)
+
+        self.segmentations, self.areas, self.boxes = preprocess_annotations(self.annotations)
+
+        self.image_id_to_image = [(image['id'], image['file_name']) for image in self.annotations['images']]
+
+    def _load_image(self, name: str):
+        image_path = self.image_paths[name]
+        image = io.imread(str(image_path))
+        return image
+
+    def _load_target(self, image_id: int):
+        segmentations = self.segmentations[image_id]
+        area = self.areas[image_id]
+        boxes = self.boxes[image_id]
+
+        masks = contours_to_masks(segmentations)
+        boxes = np.array(boxes)
+        boxes = xywh2xyxy(boxes)
+        area = np.array(area)
+
+        return masks, boxes, area
+
+    def __getitem__(self, item):
+        image_id, image_name = self.image_id_to_image[item]
+        image = self._load_image(image_name)
+
+        masks, boxes, area = self._load_target(image_id)
+
+        if self.transform:
+            categories = np.ones(shape=len(masks))
+            transformed = self.transform(image=image, masks=list(masks), bboxes=list(boxes), category_ids=categories)
+
+            image = transformed['image']
+            masks = transformed['masks']
+            boxes = transformed['bboxes']
+
+        target = {
+            'masks': torch.as_tensor(masks),
+            'boxes': torch.as_tensor(boxes, dtype=torch.float32),
+            'labels': torch.as_tensor([1] * len(masks), dtype=torch.int64),
+            'iscrowd': torch.as_tensor([1] * len(masks), dtype=torch.int64),
+            'ares': torch.as_tensor(area)
+        }
+
+        return image, target
+
+    def __len__(self):
+        return len(self.image_id_to_image)
