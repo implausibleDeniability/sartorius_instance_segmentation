077c682c184ee98d05c04bcd8a72861f55ae7ecf
Maxim Faleev <m.faleev@innopolis.university>
2021-12-14 18:50:43 +0000

    - slow version of iou

--- src/iou_metric.py
+++ src/iou_metric.py
@@ -26,29 +26,3 @@ def fast_iou(predicted_masks: np.ndarray, true_masks: np.ndarray) -> np.float64:
         thresholded_iou = n_intersects / (n_true + n_predicted - n_intersects)
         thresholded_ious.append(thresholded_iou)
     return np.mean(thresholded_ious)
-
-# DEPRECATED: use fast_iou
-def iou(true_mask: np.array, pred_mask: np.array):
-    intersection = np.logical_and(true_mask, pred_mask).sum()
-    union = np.logical_or(true_mask, pred_mask).sum() + eps
-
-    return intersection.sum() / union.sum()
-
-
-def precision(true_masks: np.array, pred_masks: np.array, iou_threshold: float):
-    true_positive = 0
-    for mask1 in true_masks:
-        for mask2 in pred_masks:
-            if iou(mask1, mask2) > iou_threshold:
-                true_positive += 1
-
-    metric = true_positive / (len(true_masks) + len(pred_masks) - true_positive)
-    return metric
-
-
-def iou_map(true_masks: np.array, pred_masks: np.array):
-    average_precision = []
-    for threshold in np.arange(start=0.5, stop=1, step=0.05):
-        average_precision.append(precision(true_masks, pred_masks, threshold))
-
-    return np.asarray(average_precision).mean()
