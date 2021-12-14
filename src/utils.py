8743cdcd734dcfa2fd7a8e3177ab4079288eb2fa
Maxim Faleev <m.faleev@innopolis.university>
2021-12-14 17:31:40 +0000

    + script to set seeds for determinism

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import os
 
 import numpy as np
+import random
+import torch
 from dotenv import load_dotenv
 
 
@@ -29,3 +31,12 @@ def get_box(mask: np.array) -> list:
     ymin = np.min(y_coords)
     ymax = np.max(y_coords)
     return [xmin, ymin, xmax, ymax]
+
+
+def make_deterministic(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
