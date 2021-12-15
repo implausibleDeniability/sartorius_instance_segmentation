186b7229c1e9a29e1d6c12b932cd6a1d79e66c76
homomorfism <arsl.sh056@mail.ru>
2021-12-15 11:12:27 +0300

    adding map metric for evaluation of model

--- src/iou_metric.py
+++ src/iou_metric.py
@@ -1,28 +1,112 @@
 import numpy as np
 
-eps = 1e-6
 
-
-def fast_iou(predicted_masks: np.ndarray, true_masks: np.ndarray) -> np.float64:
+def compute_iou(labels, y_pred):
     """
+    Computes the IoU for instance labels and predictions.
+
     Args:
-        predicted_masks: np.array [N_MASKS, HEIGHT, WIDTH]
-        true_masks: np.array [M_MASKS, HEIGHT, WIDTH]
+        labels (np array): Labels.
+        y_pred (np array): predictions
+
+    Returns:
+        np array: IoU matrix, of size true_objects x pred_objects.
     """
-    n_predicted = predicted_masks.shape[0]
-    n_true = true_masks.shape[0]
-    height = true_masks.shape[1]
-    width = true_masks.shape[2]
-    
-    predicted_masks = predicted_masks.reshape(1, n_predicted, height, width)
-    true_masks = true_masks.reshape(n_true, 1, height, width)
-    logical_or = np.logical_or(predicted_masks, true_masks).sum(axis=3).sum(axis=2)
-    logical_and = np.logical_and(predicted_masks, true_masks).sum(axis=3).sum(axis=2) + 1e-6
-    pairwise_iou = logical_and / logical_or
-    
-    thresholded_ious = []
-    for threshold in np.arange(start=0.5, stop=1, step=0.05):
-        n_intersects = np.sum(pairwise_iou >= threshold)
-        thresholded_iou = n_intersects / (n_true + n_predicted - n_intersects)
-        thresholded_ious.append(thresholded_iou)
-    return np.mean(thresholded_ious)
+
+    true_objects = len(np.unique(labels))
+    pred_objects = len(np.unique(y_pred))
+
+    # Compute intersection between all objects
+    intersection = np.histogram2d(
+        labels.flatten(), y_pred.flatten(), bins=(true_objects, pred_objects)
+    )[0]
+
+    # Compute areas (needed for finding the union between all objects)
+    area_true = np.histogram(labels, bins=true_objects)[0]
+    area_pred = np.histogram(y_pred, bins=pred_objects)[0]
+    area_true = np.expand_dims(area_true, -1)
+    area_pred = np.expand_dims(area_pred, 0)
+
+    # Compute union
+    union = area_true + area_pred - intersection
+    iou = intersection / union
+
+    return iou[1:, 1:]  # exclude background
+
+
+def precision_at(threshold, iou):
+    """
+    Computes the precision at a given threshold.
+
+    Args:
+        threshold (float): Threshold.
+        iou (np array [n_truths x n_preds]): IoU matrix.
+
+    Returns:
+        int: Number of true positives,
+        int: Number of false positives,
+        int: Number of false negatives.
+    """
+    matches = iou > threshold
+    true_positives = np.sum(matches, axis=1) >= 1  # Correct objects
+    false_negatives = np.sum(matches, axis=1) == 0  # Missed objects
+    false_positives = np.sum(matches, axis=0) == 0  # Extra objects
+    tp, fp, fn = (
+        np.sum(true_positives),
+        np.sum(false_positives),
+        np.sum(false_negatives),
+    )
+    return tp, fp, fn
+
+
+def flatten_masks(masks: np.array):
+    """Takes the array of binary masks, enumerates them and collect them in 2d mask"""
+    pred = np.zeros_like(masks[0], dtype=np.int64)
+    for ii, mask in enumerate(masks):
+        pred[mask == 1] = ii + 1
+
+    return pred
+
+
+def iou_map(truth_masks: np.array, pred_masks: np.array, verbose=0):
+    """
+    Computes the metric for the competition.
+    Masks contain the segmented pixels where each object has one value associated,
+    and 0 is the background.
+
+    Args:
+        truth_masks:  array of truth masks , e.x. (100, 520, 704)
+        pred_masks:  array of prediction masks, e.x. (200, 520, 704)
+        verbose (int, optional): Whether to print infos. Defaults to 0.
+
+    Returns:
+        float: mAP.
+    """
+    truths = flatten_masks(truth_masks)
+    preds = flatten_masks(pred_masks)
+    ious = [compute_iou(truth, pred) for truth, pred in zip(truths, preds)]
+
+    # print(ious[0].shape)
+
+    if verbose:
+        print("Thresh\tTP\tFP\tFN\tPrec.")
+
+    prec = []
+    for t in np.arange(0.5, 1.0, 0.05):
+        tps, fps, fns = 0, 0, 0
+        for iou in ious:
+            tp, fp, fn = precision_at(t, iou)
+            tps += tp
+            fps += fp
+            fns += fn
+
+        p = tps / (tps + fps + fns)
+        prec.append(p)
+
+        if verbose:
+            print("{:1.3f}\t{}\t{}\t{}\t{:1.3f}".format(t, tps, fps, fns, p))
+
+    if verbose:
+        print("AP\t-\t-\t-\t{:1.3f}".format(np.mean(prec)))
+
+    return np.mean(prec)
