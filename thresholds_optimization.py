3fba0dbcc5aff85cafe39eb7f04e185d67e10f39
Maxim Faleev <m.faleev@innopolis.university>
2021-12-16 20:19:40 +0000

    threshold optimization switched on val loader

--- thresholds_optimization.py
+++ thresholds_optimization.py
@@ -20,12 +20,11 @@ from src.iou_metric import iou_map
 def main():
     load_dotenv()
 
-    experiment_name = 'first_baseline_with_metric'
+    experiment_name = 'bsln500ep'
     weights_name = f'{experiment_name}.ckpt'
     weights_path = Path(os.environ["weights_path"]) / weights_name
 
-    global device
-    device = 'cuda:1'
+    global config
     config = EasyDict(
         dataset_path=Path(os.environ["dataset_path"]),
         device="cuda:1",
@@ -39,12 +38,12 @@ def main():
     )
 
     model = maskrcnn_resnet50_fpn(progress=False, num_classes=2)
-    model.load_state_dict(torch.load(weights_path, map_location=torch.device(device)))
-    model.to(device)
+    model.load_state_dict(torch.load(weights_path, map_location=torch.device(config.device)))
+    model.to(config.device)
     model.eval()
 
     train_dataloader = DataLoader(
-        dataset=CellDataset(cfg=config, mode="train", transform=train_transform),
+        dataset=CellDataset(cfg=config, mode="val", transform=train_transform),
         num_workers=30,
         batch_size=config.batch_size,
         collate_fn=lambda x: tuple(zip(*x)),
@@ -52,8 +51,8 @@ def main():
 
     os.makedirs('thresholds', exist_ok=True)
     study = optuna.create_study(direction='maximize')
-    for i in range(10):
-        study.optimize(lambda trial: evaluate_thresholds(trial, model, train_dataloader), n_trials=4)
+    for i in range(40):
+        study.optimize(lambda trial: evaluate_thresholds(trial, model, train_dataloader), n_trials=3)
         with open(f"thresholds/{experiment_name}.pickle", 'wb') as file:
             pickle.dump(study, file)
         study.trials_dataframe().to_csv(f"thresholds/{experiment_name}.csv", index=False)
@@ -65,7 +64,7 @@ def evaluate_thresholds(trial, model, dataloader):
     nms_thr = trial.suggest_float('nms_thr', 0.0, 1.0)
     ious = []
     for batch_idx, (images, targets) in enumerate(tqdm(dataloader)):
-        images = images2device(images, device)
+        images = images2device(images, config.device)
         outputs = model(images)
         processed_outputs = postprocess_predictions(outputs, mask_thr, score_thr, nms_thr)
         for i in range(len(images)):
