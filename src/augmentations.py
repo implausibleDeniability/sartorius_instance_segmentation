ce5401935effce6720da10814f5fb2bf10fea989
Maxim Faleev <m.faleev@innopolis.university>
2021-12-26 09:41:13 +0000

    + rotate augmentations

--- src/augmentations.py
+++ src/augmentations.py
@@ -17,3 +17,11 @@ wider_train_transform = A.Compose([
     A.Normalize(mean=(0.485,), std=(0.229,)),
     ToTensorV2(),
 ], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['category_ids']))
+
+wider2_train_transform = A.Compose([
+    A.HorizontalFlip(p=0.5),
+    A.VerticalFlip(p=0.5),
+    A.ShiftScaleRotate(border_mode=0, p=0.7, shift_limit=0.2, scale_limit=0.4),
+    A.Normalize(mean=(0.485,), std=(0.229,)),
+    ToTensorV2(),
+], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['category_ids']))
